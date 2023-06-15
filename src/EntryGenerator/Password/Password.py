a0b25afd68c855fc5bd18c191acd77a7411dc509
flizzyflo <vip3r1@gmx.de>
2023-06-15 22:37:06 +0200

    implemented password class

---
+++ src/EntryGenerator/Password/Password.py
@@ -0,0 +1,20 @@
+from src.EntryGenerator.RawPasswordError.RawPasswordError import RawPasswordError
+
+
+class Password:
+    def __init__(self, *, password: str) -> None:
+        self.password: str = None
+        self.set_password(password=password)
+
+    def set_password(self, password: str) -> None:
+        decrypted_password: str = self.decrypt_password(password=password)
+        self.password = decrypted_password
+
+    def get_password(self) -> str:
+        return self.encrypt_password()
+
+    def decrypt_password(self, *, password: str) -> str:
+        return ""
+
+    def encrypt_password(self) -> str:
+        return ""
